32d6c516053bcf35d3058233b87d25fe5c9213e2
student-programming <student-programming@yandex.ru>
2020-07-27 23:33:23 +0300

    initial project version

---
+++ moex/moex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace moex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sURL;
+            sURL = "http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.json";
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    Console.WriteLine("{0}:{1}", i, sLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
